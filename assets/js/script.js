306e4f000648a53f9ea6c4dfdb7c5cde5814eba3
ShaneWilmes <shanewilmes@yahoo.com>
2022-04-03 15:55:58 -0600

    more updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 // Targeting DOM Elements
 var currentDayEl = $('#current-day');
-var timeBlockEl = $('#time-block');
+// var timeBlockEl = $('#time-block');
+var saveButton = $('saveButton');
+
+
+
+var taskInputEL = $('#tasks');
+
+
 
 // Displays current date 
 function showToday() {
@@ -8,7 +15,26 @@ function showToday() {
     currentDayEl.text(today)
 }
 
+// Calls showToday to display the current date
 setInterval(showToday);
+
+
+
+
+
+// function printDailyTask(dailyTask) {
+//     var taskInputEL = $('<textarea>').addClass('p-2').text(dailyTask);
+
+// }
+
+
+// document.getElementById('tasks').innerHTML = function
+// var saveButton = document.getElementById('#saveButton');
+// localStorage.setItem('tasks');
+
+
+
+
 
 
 
